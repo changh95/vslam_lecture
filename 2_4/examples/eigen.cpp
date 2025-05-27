228a777362724dc4246c76bcd2312e9d1f8ddce8
Hyunggi Chang <hyunggi.chang@stradvision.com>
2025-05-28 04:26:06 +0900

    update 2_4

--- 2_4/examples/eigen.cpp
+++ 2_4/examples/eigen.cpp
@@ -21,25 +21,30 @@ int main() {
     // Essential operations for SLAM
     cout << "2. Essential SLAM Operations:" << endl;
     
-    // Rotation matrix
-    Matrix3d rotation = Matrix3d::Identity();  // 3x3 identity matrix
-    double angle = M_PI / 4;  // 45 degrees
-    rotation = AngleAxisd(angle, Vector3d::UnitZ());  // Rotation around Z axis
-    cout << "Rotation Matrix (45° around Z):\n" << rotation << "\n\n";
-
+    // Matrix creation #1 - Identity
+    Matrix3d rotation1 = Matrix3d::Identity();
+    cout << "Identity rotation matrix:\n" << rotation1 << "\n\n";
+    
+    // Matrix creation #2 - Explicit creation
+    Matrix3d rotation2;
+    rotation2 << 1, 0, 0,
+                0, 1, 0,
+                0, 0, 1;
+    cout << "Manually set rotation matrix:\n" << rotation2 << "\n\n";
+    
     // Translation vector
     Vector3d translation(1, 2, 3);
     cout << "Translation Vector:\n" << translation << "\n\n";
 
     // Transformation matrix (SE3)
     Matrix4d T = Matrix4d::Identity();
-    T.block<3,3>(0,0) = rotation;
+    T.block<3,3>(0,0) = rotation1;
     T.block<3,1>(0,3) = translation;
     cout << "Transformation Matrix (SE3):\n" << T << "\n\n";
 
     // Point transformation
     Vector3d point(1, 0, 0);
-    Vector3d transformed_point = rotation * point + translation;
+    Vector3d transformed_point = rotation1 * point + translation;
     cout << "Original Point:\n" << point << "\n\n";
     cout << "Transformed Point:\n" << transformed_point << "\n\n";
 
